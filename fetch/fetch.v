3956526e24ab25a3b06b57564748a79c5dd20211
yanbinlin-a11y <yanbinlin@cpp.edu>
2026-05-13 18:32:05 -0700

    Update fetch.v

--- fetch/fetch.v
+++ fetch/fetch.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2026/03/15 16:20:08
-// Design Name: 
-// Module Name: fetch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module fetch(
     input  wire        clk,
